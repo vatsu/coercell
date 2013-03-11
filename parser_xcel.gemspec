df96315c30838c2c276f6b28d0238798b52c73d3
Rafael Barros <rafael.barros@jazz.etc.br>
2013-03-11 19:46:05 -0300

    gem parser_xcel

---
+++ parser_xcel.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name        = 'parser_xcel'
+  s.version     = '0.0.1'
+  s.date        = '2013-02-08'
+  s.summary     = ""
+  s.description = "A parser xls/xlsx validating with model"
+  s.authors     = ["Rafael Barros"]
+  s.email       = 'rafael.barros@jazz.etc.br'
+  s.files       = ["parser_xcel.rb"]
+  s.homepage    = ''
+end
--- parser_xcel.rb
+++ parser_xcel.rb
@@ -1,48 +1,81 @@
 require 'roo'
 require 'rubygems'
 
-require File.dirname(__FILE__) + '/lib/person.rb'
-require File.dirname(__FILE__) + '/xcel.rb'
-
-if ARGV.blank? 
-	puts "Usage: ruby parcer.rb file"
-	exit
-end
-
 class ParserXcel
-
-	def initialize(xls, model)
-			
-		titles = xls.titles
-		content = xls.content(titles)
-
-		parsed = Array.new
-		errors_list = Array.new	
-
-		content.each_with_index do | p, i | 
-
-			person = model.new(p)
-
-			if person.valid?
-			
-				parsed << person
-				
-			else
-				
-				error = {} 
-				error['line'] = i	
-				error['errors']  = person.errors.full_messages
-				errors_list << error
-			end
-
-		end
+  
+  def initialize(file, model)
+    
+    @xcel = self.load_xcel(file)
+    @errors_list = Array.new
+    @parsed = Array.new
+    xcel_final = Array.new
+    content = self.content(self.titles)
+    
+    content.each_with_index do |p, i|
+      
+      person = model.new(p)
+        
+      if person.valid?
+        @parsed << person
+      else
+        error = {}
+        error['line'] = i  
+        error['errors']  = person.errors.full_messages
+        @errors_list << error
+      end
+      
+    end
+    
+    xcel_final
+    
+  end
+  
+  def data
+    @parsed.each {|parsed| parsed}
+  end
+  
+  def errors
+    @errors_list.each {|error| error}
+  end
+  
+  def load_xcel(file)
+  
+    extension = File.extname( file ).gsub( /^\./, '' ).downcase
 		
-		#unless errors_list.blank?
-		#	puts errors_list	
-		#end
-
-		puts parsed
-	end
+  	if extension == "ods"
+  		xcel = Roo::OpenOffice.new(file)
+  	elsif extension == "xls"
+  		xcel = Roo::Excel.new(file) 
+  	elsif extension == "xlsx"
+  		xcel = Roo::Excelx.new(file) 
+  	else
+  		xcel = false
+  	end
+  
+  end
+  
+  def titles(line=1)
+    titles = Array.new
+    @xcel.first_column.upto(@xcel.last_column) do |column|
+      titles << @xcel.cell(line,column)
+    end
+    titles
+  end
+  
+  def content(titles, start=2)
+  
+    body = Array.new	
+      
+  	start.upto(@xcel.last_row) do |line|
+  		content_line = {}
+	
+  	  titles.each_with_index do | title, i |
+  			content_line[title] = @xcel.cell(line, i+1)	
+  			body << content_line
+  	  end
+        
+  	end
+    body
+  end
+  
 end
-xls = Xcel.new(ARGV.first)
-parsed_xml = ParserXcel.new(xls, Person)
